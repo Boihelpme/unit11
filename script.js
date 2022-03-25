6267ec29b9c6fe70488ea6f266748e10da4feff8
Scott <jayscott@FVFZMYCBJ1WK.tld>
2022-03-25 13:38:36 -0500

    thrid commit again

--- script.js
+++ script.js
@@ -1,52 +1,65 @@
 
 
-alert(`Welcome to 20! This is competition to see
- which player gets closer to the number 20. You are 
- going to be playing agains the computer. \n\n You will draw from 1 to 10.
- The computer will stop at 16. \n\n Pay attention to the screen to see what your total is, and if you need another number, press "y".`)
+// alert(`Welcome to 20! This is competition to see
+//  which player gets closer to the number 20. You are 
+//  going to be playing agains the computer. \n\n You will draw from 1 to 10.
+//  The computer will stop at 16. \n\n Pay attention to the screen to see what your total is, and if you need another number, press "y".`)
 
-alert(`Now... Its computer's turn.`)
-
-
-// computer number
-do {
-    compc = 0;
-    userc = 0;
-    do {
-
-        var numbers = Math.floor(Math.random() * 10 ) + 1;
-        compc = compc + numbers;
-        alert(`Computer got ${numbers}. Computer new total is ${compc}`)
+// alert(`Now... Its computer's turn.`)
 
 
-    } while (compc <= 16)
+// // computer number
+// do {
+//     compc = 0;
+//     userc = 0;
+//     do {
+
+//         var numbers = Math.floor(Math.random() * 10 ) + 1;
+//         compc = compc + numbers;
+//         alert(`Computer got ${numbers}. Computer new total is ${compc}`)
 
 
-    alert("Now its your turn")
+//     } while (compc <= 16)
 
-    do { 
-        var numbers = Math.floor(Math.random() * 10 ) + 1;
-        userc = userc + numbers;
-        alert(`Your new number is ${numbers}. Your totat is ${userc}`)
-        var p1 = prompt(`Would you like to go again? y = Yes. n = No`)
+
+//     alert("Now its your turn")
+
+//     do { 
+//         var numbers = Math.floor(Math.random() * 10 ) + 1;
+//         userc = userc + numbers;
+//         alert(`Your new number is ${numbers}. Your totat is ${userc}`)
+//         var p1 = prompt(`Would you like to go again? y = Yes. n = No`)
         
-    } while ((userc <= 19) && (p1 == "y"))
+//     } while ((userc <= 19) && (p1 == "y"))
 
-    alert(`Computer: ${compc}. Human: ${userc}.`)
+//     alert(`Computer: ${compc}. Human: ${userc}.`)
 
-    if (compc <= userc) {
-        alert(` You won`)
-    } else{
-        alert(` You lost `)
-    }
+//     if (compc <= userc) {
+//         alert(` You won`)
+//     } else{
+//         alert(` You lost `)
+//     }
 
-    var p2 = prompt(`Would you like to play again? y = yes n = no`)
+//     var p2 = prompt(`Would you like to play again? y = yes n = no`)
 
-} while (p2 == "y") 
+// } while (p2 == "y") 
    
 
 
 
+function username() {
+    var user = prompt("Whats your name?")
+    return user
+}
+
+
+function rannum() {
+    var numbers = Math.floor(Math.random() * 10 ) + 1;
+    return numbers
+}
+var user = console.log(username())
+
+var numbers = console.log(rannum())
 
 
 
